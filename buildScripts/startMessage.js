6ca086812c86b8c4ed91f8c22cfe4c382eeda37b
Abayomi igwubor <abayomi.igwubor@msnacad.com>
2018-11-28 13:43:01 +0100

    eslintWorkingFine

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from'chalk';
 
-console.log(chalk.green("Starting app in dev mode dude..."))
+console.log(chalk.green("Starting app in dev mode dude...")) // eslint-disable-line no-console
